0f9ed5c1ef967c2328a824cd36347936879858de
indemaxshareresearch <indemaxshareresearch@gmail.com>
2026-05-09 10:42:27 +0530

    Html Update

--- src/app/pages/payments/payment-links/payment-links.component.ts
+++ src/app/pages/payments/payment-links/payment-links.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { PageHeaderComponent } from '../../../shared/page-header/page-header.component';
 
 @Component({
   selector: 'app-payment-links',
-  imports: [PageHeaderComponent],
+  imports: [],
   templateUrl: './payment-links.component.html',
   styleUrl: './payment-links.component.scss',
 })
